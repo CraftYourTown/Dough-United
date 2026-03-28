00b19f261b426dd1b50080dd04642780c2a1b783
luke <itslukemango@gmail.com>
2026-03-28 15:27:58 +0000

    nerf vein checks

--- dough-data/src/main/java/io/github/bakedlibs/dough/blocks/Vein.java
+++ dough-data/src/main/java/io/github/bakedlibs/dough/blocks/Vein.java
@@ -14,7 +14,7 @@ import org.bukkit.block.BlockFace;
 // TODO: Refactor this
 public final class Vein {
 
-    private static final BlockFace[] faces = new BlockFace[] { BlockFace.UP, BlockFace.DOWN, BlockFace.NORTH, BlockFace.EAST, BlockFace.SOUTH, BlockFace.WEST, BlockFace.NORTH_EAST, BlockFace.NORTH_WEST, BlockFace.SOUTH_EAST, BlockFace.SOUTH_WEST };
+    private static final BlockFace[] faces = new BlockFace[] { BlockFace.UP, BlockFace.NORTH, BlockFace.EAST, BlockFace.SOUTH, BlockFace.WEST };
 
     private Vein() {}
 
